7d67daccf26b9fcf557edad23e524a01d4b707b2
Panumas Srisook (Moss) <panumas.s@thtal.com>
2020-03-23 11:52:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { Header } from "./components/Header";
+import { Blance } from "./components/Blance";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header />
+      <div className="container">
+        <Blance />
+      </div>
     </div>
   );
 }
---
+++ src/components/Blance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Blance = () => {
+    return (
+        <>
+            <h4>Your Blance</h4>
+            <h1 id='blance'>$0.00</h1>
+        </>
+    )
+}
+
